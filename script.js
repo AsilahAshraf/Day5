e5aa43a0e8892540a1bfa596fc71f4a212a6d9a7
AsilahAshraf <asilahashraf866@gmail.com>
2024-05-17 11:23:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function matchpass(){
+    var firstPass=document.f1.password.value;
+    var secondPass=document.f1.password2.value;
+    if(firstPass==secondPass){
+        alert("your password is correct");
+        return true;
+    }
+    else{
+        alert("password must be same");
+        return false;
+    }
+
+}
